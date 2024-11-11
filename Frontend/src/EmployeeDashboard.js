e87918626c5b1ef6003c2749993842dd0ebf49c7
SethDVS <sethdvs223@gmail.com>
2024-11-11 21:49:03 +0200

    sonarcube fixes

--- Frontend/src/EmployeeDashboard.js
+++ Frontend/src/EmployeeDashboard.js
@@ -1,21 +1,19 @@
-import React, { useContext, useState, useEffect }  from "react";
+import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";  
 import { ThemeContext } from './ThemeContext';
 
 function EmployeeDashboard() {
-  const [setMessage] = useState("");
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   const navigate = useNavigate();
+
   useEffect(() => {
     const accountNumber = localStorage.getItem("accNo");
     const userType = localStorage.getItem("userType");
-    if (!accountNumber) {
-      navigate("/login");  
-    }
-    if (userType !="Employee") {
+    if (!accountNumber || userType !== "Employee") {
       navigate("/login");  
     }
   }, [navigate]); 
+
   return (
     <div>
       <h2>Welcome to the Employee Dashboard</h2>
--- Frontend/src/UserDashboard.js
+++ Frontend/src/UserDashboard.js
@@ -1,22 +1,19 @@
-import React, { useState, useEffect, useContext }  from "react";
+import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";  
 import { ThemeContext } from './ThemeContext';
 
-
 function UserDashboard() {
-  const [setMessage] = useState("");
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   const navigate = useNavigate();
+
   useEffect(() => {
     const accountNumber = localStorage.getItem("accNo");
     const userType = localStorage.getItem("userType");
-    if (!accountNumber) {
-      navigate("/login");  
-    }
-    if (userType != "User") {
+    if (!accountNumber || userType !== "User") {
       navigate("/login");  
     }
   }, [navigate]); 
+
   return (
     <div>
       <h2>Welcome to the Dashboard</h2>
